fb46cad8aee35bf1d3b11c7c2e52bd753ebf16a3
codepasty <75074598+codepasty@users.noreply.github.com>
2022-07-15 13:24:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+
+        function muSic(){
+            var x = document.getElementById("myTopnav");
+if (x.className === "responsive"){
+    x.className = "topnav";
+}else {
+    x.className = "topnav";
+}
+        }
+
+        function synCs(){
+            var x = document.getElementById("myTopnav");
+if (x.className === "responsive"){
+    x.className = "topnav";
+}else {
+    x.className = "topnav";
+}
+        }
+
+function conTact(){
+            var x = document.getElementById("myTopnav");
+if (x.className === "responsive"){
+    x.className = "topnav";
+}else {
+    x.className = "topnav";
+}
+        }
+        
